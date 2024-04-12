269c9dcdf28a0270ba5ebdc655457f7992dd32e8
Berker ERTAN <berkerertan@hotmail.com>
2024-04-12 08:32:58 +0300

    New interceptors and services added.

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,3 +1,7 @@
+// Kullanıcının yetkilendirme bilgilerini (örneğin, JWT token'ı) her isteğe otomatik
+//  olarak eklemek için kullanılır. Bu sayede, her HTTP isteğinde yetkilendirme 
+//  bilgilerini tekrar tekrar eklemekten kaçınılır.
+
 import { HttpInterceptorFn } from '@angular/common/http';
 import { LocalStorageService } from '../../features/services/concretes/local-storage.service';
 import { inject } from '@angular/core';
